d75b6a03e41ef9b56ed7119afcb1d925d54ceef6
John4E656F <jm49andres@gmail.com>
2022-11-01 16:06:57 +0100

    done responsive hero

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -12,17 +12,20 @@ import {
 } from '@mui/material'
 
 const Hero = () => {
-    const HeroTextContainer = styled('div')(({theme}) => ({
 
-    }))
     return(
         <Grid container justifyContent="center"  sx={{  }}>
             <BgVideo />
-            <Grid item lg={4} md={6} bg={6} sm={6} xs={8}
-                sx={{ mt: { xs: 5, sm: 5, bg: 15 } }}>
-                <Grid sx={{ textAlign: 'center', padding: '10px 10px 10px 10px', borderRadius: '5px', opacity: '70%', backgroundColor: '#B8B8B8', width: 'auto'}}>
-                    <Typography variant='h3'>Take your event to the next level</Typography>
-                    <Button>Hello</Button>
+            <Grid item lg={4} md={6} sm={6} xs={8}
+                sx={{ mt: { xs: 2, sm: 10, md: 15, lg: 20 } }}>
+                <Grid sx={{ textAlign: 'center', padding: '10px 10px 10px 10px', borderRadius: '5px', opacity: '70%', backgroundColor: '#B8B8B8', width: 'auto',}}>
+                    <Typography variant='h3' 
+                        sx={{ 
+                            fontSize: { xs: '16px', sm: '20px', md: '26px' },
+                            fontStyle: 'bold',
+                            mb: 2
+                        }}>Take your event to the next level</Typography>
+                    <Button variant="contained" >Hello</Button>
                 </Grid>
             </Grid>
         </Grid>
