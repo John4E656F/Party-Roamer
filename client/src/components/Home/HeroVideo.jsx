b9fd08d19d034f95e1024a5974429415adc44c39
John4E656F <jm49andres@gmail.com>
2022-11-05 16:45:15 +0100

    changing hero

---
+++ client/src/components/Home/HeroVideo.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//Import Assets
+import Video from '../../assets/heroVideo.mp4'
+import {
+    Box,
+    Grid,
+} from '@mui/material'
+import { useTheme } from '@mui/material/styles';
+
+const HeroVideo = () => {
+    const theme = useTheme();
+
+    return(
+        <Grid container sx={{ position: 'static', overflow: 'hidden', padding: { xs: '0 1.5em 0 1.5em', sm: '0 1.5em 0 1.5em', md: '0 8.5em 0 8.5em' }}}>
+            <video autoPlay loop muted className="video" style={{ position: 'absolute'}}>
+                <source src={Video} type='video/mp4' />
+                Your browser does not support the video tag.
+            </video>
+        </Grid>
+    )
+}
+
+export default HeroVideo;
